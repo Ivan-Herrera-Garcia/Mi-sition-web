d1a6eb9cec4ba8103d911f3faea89223a5626542
Ivan Herrera (Bellkhen) <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2023-04-07 22:34:51 -0600

    Creacion del sitio web personal

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+/*let saludo = new Date();
+var hora = saludo.getHours();
+if (hora >= 12) {
+    document.body.innerHTML = "<h1>Buenas tardes Chavales</h1>";
+} else {
+    document.body.innerHTML = "<h1>Buenos dias Chavales</h1>";
+}
+
+const imagenes = document.createElement("section");
+imagenes.src="javascript.png";
+imagenes.src="python.png";
+imagenes.src="swift.jpg";
+imagenes.src="certificados.jpg";
+
+document.body.appendChild(imagenes);*/
