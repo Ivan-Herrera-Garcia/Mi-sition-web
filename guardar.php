8a6749ce5f5ee8b9a07e2afb93b927c9ce22e943
Ivan Herrera (Bellkhen) <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2023-04-12 23:05:21 -0600

    Correccion

--- guardar.php
+++ guardar.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "aws.connect.psdb.cloud";
-$username = "lcry2avnbkc3qbr4qlux";
-$password = "pscale_pw_1UzcNtcXUjikr4Ta0sFWB13klnUrTbcrKWLJ6ubn2qo";
+$username = "mpqvkn2y24j8c6rs2h0i";
+$password = "pscale_pw_yWUXbGVGqc2hBuntQsAJJ6IqhQEj5P1yF7H48iBegTk";
 $dbname = "sitio-web";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
